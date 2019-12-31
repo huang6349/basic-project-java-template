235b71262ca28b0981a46c46c1ac4e1b24bfde85
huang6349 <252683858@qq.com>
2019-12-31 18:59:25 +0800

    :wrench: 增加 @ant-design/pro-layout 的支持

--- client/src/layouts/index.js
+++ client/src/layouts/index.js
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import ProLayout from '@ant-design/pro-layout';
+import styles from './index.css';
 
 const BasicLayout = ({ children }) => {
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <ProLayout
+      className={styles['layout']}
+      menuHeaderRender={!1}
+      layout="sidemenu"
+      contentWidth="Fluid"
+      navTheme="light"
+      fixedHeader={!0}
+      fixSiderbar={!0}
+    >
+      <React.Fragment>{children}</React.Fragment>
+    </ProLayout>
+  );
 };
 
 export default BasicLayout;
