18206b0ff32994dfbfc887e9ab14855330927132
huang6349 <252683858@qq.com>
2025-05-26 17:42:17 +0800

    :hammer: 重大重构

---
+++ project-ai/src/main/java/org/myframework/ai/config/FrameworkMcp.java
@@ -0,0 +1,9 @@
+package org.myframework.ai.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class FrameworkMcp {
+}
