b50324c87d9ad3d041299d1a0d96d64bbe36cae2
huang6349 <252683858@qq.com>
2025-05-30 16:26:04 +0800

    :hammer: 重大重构

--- project-framework/src/main/java/org/myframework/extra/dict/DictHandler.java
+++ project-framework/src/main/java/org/myframework/extra/dict/DictHandler.java
@@ -2,12 +2,9 @@ package org.myframework.extra.dict;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
-import static cn.hutool.core.text.CharSequenceUtil.removeSuffix;
-import static cn.hutool.core.util.ClassUtil.getPackage;
 import static cn.hutool.core.util.ClassUtil.scanPackageByAnnotation;
 
 @AllArgsConstructor
@@ -17,8 +14,7 @@ public class DictHandler implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() {
-        val packageName = removeSuffix(getPackage(getClass()), ".extra.dict");
-        scanPackageByAnnotation(packageName, Dict.class).stream()
+        scanPackageByAnnotation(null, Dict.class).stream()
                 .filter(clazz -> clazz.isEnum() && EnumDict.class.isAssignableFrom(clazz))
                 .map(DictUtil::parse)
                 .forEach(DictCache::add);
