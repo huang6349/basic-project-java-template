29769b4e89ee94698e30836a5c9f17fb6d39d8a1
huang6349 <252683858@qq.com>
2025-05-31 19:13:29 +0800

    :hammer: 重大重构

--- project-framework/src/main/java/org/myframework/extra/dict/DictHandler.java
+++ project-framework/src/main/java/org/myframework/extra/dict/DictHandler.java
@@ -12,9 +12,11 @@ import static cn.hutool.core.util.ClassUtil.scanPackageByAnnotation;
 @Component
 public class DictHandler implements InitializingBean {
 
+    static final String EMPTY = "";
+
     @Override
     public void afterPropertiesSet() {
-        scanPackageByAnnotation(null, Dict.class).stream()
+        scanPackageByAnnotation(EMPTY, Dict.class).stream()
                 .filter(clazz -> clazz.isEnum() && EnumDict.class.isAssignableFrom(clazz))
                 .map(DictUtil::parse)
                 .forEach(DictCache::add);
